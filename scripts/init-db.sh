d66dc64e6d79460226ef253c87201a3da852d97c
Abdollah GADER <abdollahgader75@gmail.com>
2026-04-14 03:16:25 +0200

    fix: add GRANT CREATE ON SCHEMA public for PG 15+ compatibility

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -5,7 +5,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE ROLE schlass_migrations WITH LOGIN PASSWORD 'schlass_migrations';
     CREATE ROLE schlass_app WITH LOGIN PASSWORD 'schlass_app';
 
-    GRANT ALL PRIVILEGES ON DATABASE schlass TO schlass_migrations;
-    GRANT CONNECT ON DATABASE schlass TO schlass_app;
+    GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO schlass_migrations;
+    GRANT CREATE ON SCHEMA public TO schlass_migrations;
+    GRANT CONNECT ON DATABASE $POSTGRES_DB TO schlass_app;
     GRANT USAGE ON SCHEMA public TO schlass_app;
 EOSQL
